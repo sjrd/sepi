055d487c88d4451c189c8a2d6c1253b7614e87cd
Sébastien Doeraene <sjrdoeraene@gmail.com>
2007-10-21 12:34:21 +0000

    Ajout des informations de licence GNU General Public License.

--- source/run/SepiCore/SepiCore.pas
+++ source/run/SepiCore/SepiCore.pas
@@ -1,3 +1,22 @@
+{-------------------------------------------------------------------------------
+Sepi - Object-oriented script engine for Delphi
+Copyright (C) 2006-2007  Sébastien Doeraene
+All Rights Reserved
+
+This file is part of Sepi.
+
+Sepi is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Sepi is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Sepi.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------}
+
 {*
   Définit les classes coeur de Sepi
   @author sjrd
