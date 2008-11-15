6d6626e14e3eb1619065f7f5099c5dd61507c745
Sébastien Doeraene <sjrdoeraene@gmail.com>
2008-11-15 21:37:24 +0000

    SepiImportAll.bat modifié pour transmettre ses paramètres à ses appels de SepiDelphiUnitImporter.

--- bin/SepiDelphiUnitImporter/SepiImportAll.bat
+++ bin/SepiDelphiUnitImporter/SepiImportAll.bat
@@ -1,12 +1,12 @@
 @SET RESDIR=$(SEPISDK)\resources\
 @SET OUTDIR=$(SEPISDK)\source\run
 
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Types.pas
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Windows.pas --lazy-load=true --exclude-routines
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ SepiRTLSysImport.csv
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiWinTypesImport\ SepiWinTypesImport.csv --lazy-load=true --exclude-routines
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLCommonImport\ SepiRTLCommonImport.csv
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSCLImport\ SepiSCLImport.csv
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiVCLStdImport\ SepiVCLStdImport.csv
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSVCLImport\ SepiSVCLImport.csv
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSDLImport\ SepiSDLImport.csv
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Types.pas %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Windows.pas --lazy-load=true --exclude-routines %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ SepiRTLSysImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiWinTypesImport\ SepiWinTypesImport.csv --lazy-load=true --exclude-routines %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLCommonImport\ SepiRTLCommonImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSCLImport\ SepiSCLImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiVCLStdImport\ SepiVCLStdImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSVCLImport\ SepiSVCLImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
+SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSDLImport\ SepiSDLImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
