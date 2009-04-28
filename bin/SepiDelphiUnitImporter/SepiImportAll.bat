a5fbcf365feaee351f086c64eb2bc6cb897fc732
Sébastien Doeraene <sjrdoeraene@gmail.com>
2009-04-28 23:31:50 +0000

    Nouveau script SepiImportAlmostAll.bat qui importe tout sauf Types.pas et Windows.pas. Il est plus rapide, mais ne doit servir que pour des tests répététifs.

--- bin/SepiDelphiUnitImporter/SepiImportAll.bat
+++ bin/SepiDelphiUnitImporter/SepiImportAll.bat
@@ -3,10 +3,5 @@
 
 SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Types.pas %1 %2 %3 %4 %5 %6 %7 %8 %9
 SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Windows.pas --lazy-load=true --exclude-routines %1 %2 %3 %4 %5 %6 %7 %8 %9
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ SepiRTLSysImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiWinTypesImport\ SepiWinTypesImport.csv --lazy-load=true --exclude-routines %1 %2 %3 %4 %5 %6 %7 %8 %9
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLCommonImport\ SepiRTLCommonImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSCLImport\ SepiSCLImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiVCLStdImport\ SepiVCLStdImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSVCLImport\ SepiSVCLImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
-SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiSDLImport\ SepiSDLImport.csv %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+SepiImportAlmostAll
