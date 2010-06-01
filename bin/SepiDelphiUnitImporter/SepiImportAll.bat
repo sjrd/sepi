25e44db02d3e5e4717961fcedfaab1af5ee0620d
Sébastien Doeraene <sjrdoeraene@gmail.com>
2010-06-01 17:46:00 +0000

    Correction d'un bug dans SepiImportAll.bat.

--- bin/SepiDelphiUnitImporter/SepiImportAll.bat
+++ bin/SepiDelphiUnitImporter/SepiImportAll.bat
@@ -4,4 +4,4 @@
 SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Types.pas %1 %2 %3 %4 %5 %6 %7 %8 %9
 SepiDelphiUnitImporter -r %RESDIR% -o %OUTDIR%\SepiRTLSysImport\ Windows.pas --lazy-load=true --exclude-routines %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-SepiImportAlmostAll
+SepiImportAlmostAll %1 %2 %3 %4 %5 %6 %7 %8 %9
